2df235b6feba42b2fcce192a7427dfb434bb6e64
romsson <romain.vuillemot@gmail.com>
2016-01-27 15:57:07 +0100

    Fixing link

--- modules/vistk.py
+++ modules/vistk.py
@@ -16,8 +16,8 @@ class VisTkViz(object):
     JS_LIBS = ['https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min.js',
                #'https://cdnjs.cloudflare.com/ajax/libs/queue-async/1.0.7/queue.min.js',
                'http://cid-harvard.github.io/vis-toolkit/js/topojson.js',
-               # 'http://cid-harvard.github.io/vis-toolkit/build/vistk.js']
-               'http://127.0.0.1/rv/Dev/vis-toolkit/build/vistk.js']
+               'http://cid-harvard.github.io/vis-toolkit/build/vistk.js']
+               #'http://127.0.0.1/rv/Dev/vis-toolkit/build/vistk.js']
 
     def create_container(self):
         container_id = "vistk_div_{id}".format(id=random.randint(0, 100000))
