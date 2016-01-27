54961638fd6f998568410b4d0aba5fbb9fb2bdef
romsson <romain.vuillemot@gmail.com>
2016-01-27 18:14:55 +0100

    Cleanup

--- modules/vistk.py
+++ modules/vistk.py
@@ -14,10 +14,8 @@ __opacity = 1
 class VisTkViz(object):
 
     JS_LIBS = ['https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min.js',
-               #'https://cdnjs.cloudflare.com/ajax/libs/queue-async/1.0.7/queue.min.js',
                'http://cid-harvard.github.io/vis-toolkit/js/topojson.js',
                'http://cid-harvard.github.io/vis-toolkit/build/vistk.js']
-               #'http://127.0.0.1/rv/Dev/vis-toolkit/build/vistk.js']
 
     def create_container(self):
         container_id = "vistk_div_{id}".format(id=random.randint(0, 100000))
@@ -196,8 +194,6 @@ class TreemapColor(VisTkViz):
           var viz_data = %s;
           var viz_container = '#%s';
 
-          //d3.select(viz_container).style('position', 'absolute');
-
           visualization = vistk.viz()
                 .params({
                   type: 'treemap',
@@ -367,7 +363,7 @@ class Caterplot(VisTkViz):
 
           var viz_data = %s;
           var viz_container = '#%s';
-          console.log("DATA", viz_data)
+
           var visualization = vistk.viz()
             .params({
               type: 'caterplot',
